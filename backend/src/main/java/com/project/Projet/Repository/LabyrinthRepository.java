bb917c36ed91350e3561fee4df8ceae94d2d19df
DELL <karimaamiri3002@gmail.com>
2025-12-18 23:27:34 +0100

    remove Labyrinth-related files and references, add Point and PointSample entities and repositories

--- backend/src/main/java/com/project/Projet/Repository/LabyrinthRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.Projet.Repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.Projet.Entity.Labyrinth;
-
-public interface LabyrinthRepository extends JpaRepository<Labyrinth, Long> {
-	}
